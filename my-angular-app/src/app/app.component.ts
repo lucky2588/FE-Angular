aaeec895f3cc868158ed249d50c13c1a5276c45a
lucky2588 <duwcsthang2001@gmail.com>
2023-08-11 17:36:02 +0700

    Create app.component.ts

---
+++ my-angular-app/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLinkActive } from '@angular/router';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+
+  title = 'my-angular-app';
+  constructor(){
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+
+
+
+}
